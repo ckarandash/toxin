24871e56b7eb8410fd8f5b3c4c15e620961ed5cf
Mikhail Kondrashov <mike.kond.02@mail.ru>
2024-03-22 14:08:00 +0300

    feat(components/form-elements/choice-field): add choice-field component ui
    This is component for radios, checkboxes (including rich) and toggles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import '@components/form-elements/text-field';
 import '@components/form-elements/dropdown';
 import '@components/form-elements/range-slider';
+import '@components/form-elements/choice-field';
