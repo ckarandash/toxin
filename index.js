04b1e4aa65474aa9f96bf0c27decc69b5233a767
Mikhail Kondrashov <mike.kond.02@mail.ru>
2024-03-23 08:59:17 +0300

    feat(components/form-elements/btn): add button component

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ import '@components/form-elements/range-slider';
 import '@components/form-elements/choice-field';
 import '@components/form-elements/like-btn';
 import '@components/form-elements/rate-btn';
+import '@components/form-elements/btn';
