2818054f37bf2990bb7c7d7a4786f7e8046ccd28
Mikhail Kondrashov <mike.kond.02@mail.ru>
2024-04-23 09:50:41 +0300

    fix: remove unused js file

--- index.js
+++ index.js
@@ -20,5 +20,4 @@ import '@components/cards/registration-form';
 import '@components/cards/search-room-form';
 import '@components/cards/book-form';
 
-import '@components/header';
 import '@components/header-mobile';
